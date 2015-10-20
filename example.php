78ebdcd04f196a7931c7db6aa5ba0d25e22fa328
valuex-ua <alexander.mcgarret@gmail.com>
2015-10-20 20:27:02 +0300

    Generated composer.json & basic example

---
+++ example.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * example.php
+ *
+ * @author Alexander Fedyashov <af@e42.guru>
+ */
+
+require 'vendor/autoload.php';
+
+use LayerShifter\TLDExtract\Extract;
+
+Extract::extract('http://user@google.com/search');
+//Extract::extract('http://[1080:0:0:0:8:800:200C:417A]/index.html');
+//Extract::extract('http://[1080:0:0:0:8:800:200C:417A]:90/index.html');
+//Extract::extract('http://pikabu.ru:96/index.html');
