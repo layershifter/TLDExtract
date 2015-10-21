508940f395695eb7a3d9ac6f731be4baed1524dd
valuex-ua <alexander.mcgarret@gmail.com>
2015-10-21 18:39:20 +0300

    function EndsWith

--- src/Helpers.php
+++ src/Helpers.php
@@ -8,6 +8,12 @@
 namespace LayerShifter\TLDExtract;
 
 
+/**
+ * Class Helpers
+ * @package LayerShifter\TLDExtract
+ *
+ * Includes static functions (helpers) for package
+ */
 class Helpers
 {
 
@@ -22,5 +28,18 @@ class Helpers
     public static function startsWith($haystack, $needle)
     {
         return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
+    }
+
+    /**
+     * Implementation endsWith() from http://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
+     *
+     * @param string $haystack
+     * @param string $needle
+     *
+     * @return bool
+     */
+    public static function endsWith($haystack, $needle)
+    {
+        return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
     }
 }
