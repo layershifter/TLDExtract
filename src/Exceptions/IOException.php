bb4c0000ce9a11c7944f504f26283d432b68193e
valuex-ua <alexander.mcgarret@gmail.com>
2015-10-23 21:47:30 +0300

    Ready to release Exceptions

--- src/Exceptions/IOException.php
+++ src/Exceptions/IOException.php
@@ -1,28 +1,52 @@
 <?php
 /**
- * IOException.php
+ * PHP version 5
  *
- * @author Alexander Fedyashov <a@fedyashov.com>
+ * @category Exceptions
+ * @package  LayerShifter/TLDExtract/Exceptions
+ * @author   Alexander Fedyashov <a@fedyashov.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/layershifter/TLDExtract
  */
 
 namespace LayerShifter\TLDExtract\Exceptions;
 
 
+/**
+ * Exception for filesystem errors
+ *
+ * @category Exceptions
+ * @package  LayerShifter/TLDExtract/Exceptions
+ * @author   Alexander Fedyashov <a@fedyashov.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/layershifter/TLDExtract
+ */
 class IOException extends \Exception
 {
-    private $filename;
+    private $_filename;
 
-    public function __construct($message, $code = 0, \Exception $previous = null, $filename = null)
-    {
-        $this->filename = $filename;
+    /**
+     * Constructor of exception.
+     *
+     * @param string     $message  Message for exception
+     * @param int        $code     Error code
+     * @param \Exception $previous Parent exception
+     * @param null       $filename Filename
+     */
+    public function __construct(
+        $message, $code = 0, \Exception $previous = null, $filename = null
+    ) {
+        $this->_filename = $filename;
         parent::__construct($message, $code, $previous);
     }
 
     /**
+     * Gets filename that caused error
+     *
      * @return string
      */
     public function getFilename()
     {
-        return $this->filename;
+        return $this->_filename;
     }
 }
--- src/Exceptions/ListException.php
+++ src/Exceptions/ListException.php
@@ -1,13 +1,26 @@
 <?php
 /**
- * ListException.php
+ * PHP version 5
  *
- * @author Alexander Fedyashov <a@fedyashov.com>
+ * @category Exceptions
+ * @package  LayerShifter/TLDExtract/Exceptions
+ * @author   Alexander Fedyashov <a@fedyashov.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/layershifter/TLDExtract
  */
 
 namespace LayerShifter\TLDExtract\Exceptions;
 
 
+/**
+ * Exception for errors with TLD list
+ *
+ * @category Exceptions
+ * @package  LayerShifter/TLDExtract/Exceptions
+ * @author   Alexander Fedyashov <a@fedyashov.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/layershifter/TLDExtract
+ */
 class ListException extends \Exception
 {
 }
