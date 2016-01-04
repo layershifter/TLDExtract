29a8b6a108aa8c30837843133f266dc4c5a2c369
layershifter <alexander.mcgarret@gmail.com>
2016-01-04 22:49:33 +0200

    Result's interface

---
+++ src/Interfaces/ResultInterface.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * PHP version 5.
+ *
+ * @category Interfaces
+ * @package  LayerShifter/TLDExtract
+ * @author   Alexander Fedyashov <a@fedyashov.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/layershifter/TLDExtract
+ * @since    Version 0.2.0
+ */
+
+namespace LayerShifter\TLDExtract\Interfaces;
+
+interface ResultInterface
+{
+
+    /**
+     * Class that implements ResultInterface must have following constructor.
+     *
+     * @param $subdomain
+     * @param $domain
+     * @param $tld
+     */
+    public function __construct($subdomain, $domain, $tld);
+}
