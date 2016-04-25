c1ba0a8764f17626218d57cdcc5dc9f724eec2d4
Alexander Fedyashov <a@fedyashov.com>
2016-04-25 21:16:17 +0300

    Copyright update

--- src/Exceptions/Exception.php
+++ src/Exceptions/Exception.php
@@ -1,26 +1,11 @@
 <?php
 /**
- * The MIT License (MIT)
+ * TLDExtract: Domain parser library.
  *
- * Copyright (c) 2016 Alexander Fedyashov
+ * @link      https://github.com/layershifter/TLDExtract
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * @copyright Copyright (c) 2016, Alexander Fedyashov
+ * @license   https://raw.githubusercontent.com/layershifter/TLDExtract/master/LICENSE MIT License
  */
 
 namespace LayerShifter\TLDExtract\Exceptions;
--- src/Exceptions/IOException.php
+++ src/Exceptions/IOException.php
@@ -1,26 +1,11 @@
 <?php
 /**
- * The MIT License (MIT)
+ * TLDExtract: Domain parser library.
  *
- * Copyright (c) 2016 Alexander Fedyashov
+ * @link      https://github.com/layershifter/TLDExtract
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * @copyright Copyright (c) 2016, Alexander Fedyashov
+ * @license   https://raw.githubusercontent.com/layershifter/TLDExtract/master/LICENSE MIT License
  */
 
 namespace LayerShifter\TLDExtract\Exceptions;
@@ -30,15 +15,18 @@ namespace LayerShifter\TLDExtract\Exceptions;
  */
 class IOException extends Exception
 {
+    /**
+     * @var null|string Filename
+     */
     private $filename;
 
     /**
      * Constructor of exception.
      *
-     * @param string     $message  Message for exception
-     * @param int        $code     Error code
-     * @param \Exception $previous Parent exception
-     * @param null       $filename Filename
+     * @param string      $message  Message for exception
+     * @param int         $code     Error code
+     * @param \Exception  $previous Parent exception
+     * @param null|string $filename Filename
      */
     public function __construct($message, $code = 0, \Exception $previous = null, $filename = null)
     {
@@ -49,7 +37,7 @@ class IOException extends Exception
     /**
      * Gets filename that caused error.
      *
-     * @return string
+     * @return null|string
      */
     public function getFilename()
     {
