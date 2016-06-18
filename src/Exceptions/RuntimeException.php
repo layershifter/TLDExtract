adf5afa5436245754e48504ab8840397e4d06c0c
Alexander Fedyashov <a@fedyashov.com>
2016-06-18 11:27:36 +0300

    Update copyright and remove unnecessary exceptions

--- src/Exceptions/RuntimeException.php
+++ src/Exceptions/RuntimeException.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * TLDExtract: Domain parser library.
+ * TLDExtract: Library for extraction of domain parts e.g. TLD. Domain parser that uses Public Suffix List.
  *
  * @link      https://github.com/layershifter/TLDExtract
  *
  * @copyright Copyright (c) 2016, Alexander Fedyashov
- * @license   https://raw.githubusercontent.com/layershifter/TLDExtract/master/LICENSE MIT License
+ * @license   https://raw.githubusercontent.com/layershifter/TLDExtract/master/LICENSE Apache 2.0 License
  */
 
 namespace LayerShifter\TLDExtract\Exceptions;
